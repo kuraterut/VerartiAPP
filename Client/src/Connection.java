b5e390cac7b299b22249df8dea375b19ea3e5a74
kuraterut <kurylilia@gmail.com>
2024-10-26 18:49:24 +0300

    Client: create SideBar, Calendar interface

---
+++ Client/src/Connection.java
@@ -0,0 +1,6 @@
+import java.util.*;
+
+public class Connection{
+	public static boolean connection;
+	
+}
--- Client/src/Help.java
+++ Client/src/Help.java
@@ -1,72 +1,55 @@
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
-
-import java.sql.*;
-import java.nio.file.*;
-import java.io.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import java.util.*;
-import java.time.*;
-import java.time.format.*;
-import java.net.*;
-import java.awt.*;
 
 
-class EncryptionUtil {
-   private static final String ALGORITHM = "AES";
-   private static final String TRANSFORMATION = "AES";
+public class Help extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane gridPane = new GridPane();
+        int rows = 5;
+        int cols = 5;
 
-   // Ваш фиксированный ключ (должен быть 16 байт для AES-128)
-   private static final String SECRET_KEY = "1549713486237445"; // Замените на ваш ключ
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                // Создаем прямоугольник (ячейку)
+                Rectangle cell = new Rectangle(100, 100, Color.TRANSPARENT); // Прозрачный
+                final int roww = row;
+                final int coll = col;
+                cell.setOnMouseClicked(event -> showCellInfo(roww, coll));
 
-   public static String encrypt(String data) throws Exception {
-       SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
-       Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-       cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-       byte[] encryptedData = cipher.doFinal(data.getBytes());
-       return Base64.getEncoder().encodeToString(encryptedData);
-   }
+                // Создаем текст для отображения в ячейке
+                Text text = new Text("R: " + row + "\nC: " + col);
+                text.setFill(Color.BLACK); // Цвет текста
 
-   public static String decrypt(String encryptedData) throws Exception {
-       SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
-       Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-       cipher.init(Cipher.DECRYPT_MODE, secretKey);
-       byte[] decryptedData = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
-       return new String(decryptedData);
-   }
+                // Добавляем элементы в GridPane
+                gridPane.add(text, col, row); // Текст поверх прямоугольника
+                gridPane.add(cell, col, row);
+            }
+        }
 
-}
+        Scene scene = new Scene(gridPane, 600, 600);
+        primaryStage.setTitle("Clickable Grid with Text");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-public class Help{
+    private void showCellInfo(int row, int col) {
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setTitle("Cell Information");
+        alert.setHeaderText(null);
+        alert.setContentText("You clicked on cell at Row: " + row + ", Column: " + col);
+        alert.showAndWait();
+    }
+
     public static void main(String[] args) {
-        try{
-            System.out.println(EncryptionUtil.encrypt("jdbc:mysql://95.165.99.218:3306/VeronichkaNailsApp?serverTimezone=Europe/Moscow&useSSL=false&allowPublicKeyRetrieval=true"));
-        }
-        catch(Exception ex){
-            System.out.println(ex);
-        }
-   }
-
-   public static void loadDatabaseProperties() {
-       try {
-           // Предположим, что вы извлекли зашифрованные данные из файла
-           String encryptedUrl = "..."; // Получите зашифрованный URL
-           String encryptedUsername = "..."; // Получите зашифрованный username
-           String encryptedPassword = "..."; // Получите зашифрованный password
-
-           // Дешифрование данных
-           String dbUrl = EncryptionUtil.decrypt(encryptedUrl);
-           String username = EncryptionUtil.decrypt(encryptedUsername);
-           String password = EncryptionUtil.decrypt(encryptedPassword);
-
-           // Используйте эти данные для подключения к базе данных
-           System.out.println("DB URL: " + dbUrl);
-           System.out.println("Username: " + username);
-           System.out.println("Password: " + password);
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
-   }
+        launch(args);
+    }
 }
